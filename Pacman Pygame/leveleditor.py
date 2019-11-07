47f3ee65e58c9e35782d57a8e9c9c1f76a6b5829
Alex Giberson <alexgiberson7@gmail.com>
2019-11-07 15:43:49 -0500

    added powerup so you can eat the ghosts, and made it so that you can put powerup blocks down in the editor

--- Pacman Pygame/leveleditor.py
+++ Pacman Pygame/leveleditor.py
@@ -17,11 +17,12 @@ yellow = (255,255,0)
 black = (0,0,0)
 
 value = 'barrier'
+arrowIncrement = 0
+arrowIncrements = [2, 10, 20]
 x = 0
 y = 0
 xy = []
 accept = False
-
 
 
 edit = input("Type level name to edit. To create new, leave blank: ")
@@ -51,66 +52,94 @@ while exit == False:
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			exit = True
-	if event.type == pygame.KEYDOWN:
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			if pygame.mouse.get_pos()[0]%2 == 0:
+				x=pygame.mouse.get_pos()[0]
+				y=pygame.mouse.get_pos()[1]
+			else:
+				x=pygame.mouse.get_pos()[0] + 1
+				y=pygame.mouse.get_pos()[1] + 1
+
+		if event.type == pygame.KEYDOWN:
 		#print(str(event.key))
-		if event.key == pygame.K_LEFT:
-			x += -20
-			#print(str(x)+", "+str(y))
-		elif event.key == pygame.K_RIGHT:
-			x += 20
-			#print(str(x)+", "+str(y))
-		elif event.key == pygame.K_UP:
-			y += -20
-			#print(str(x)+", "+str(y))
-		elif event.key == pygame.K_DOWN:
-			y += 20
-		if event.key == pygame.K_c:
-			if (value,x,y,rectwidth,rectheight) not in xy:
-				xy.append((value,x,y,rectwidth,rectheight))
-				print("Block saved to list")
-		if event.key == pygame.K_r:
-			while not accept:
-				try:
-					print("Resize block: ")
-					rectwidth = int(input("Enter rect width: "))
-					rectheight = int(input("Enter rect height: "))
-					accept = True
-				except:	
-					print("Invalid!")
-			accept = False
-		if event.key == pygame.K_z:
-			if len(xy) > 1:
-				print(str(xy.pop(-1))+" was removed from list")
-		if event.key == pygame.K_g:
-			if value == 'barrier':
-				value = 'food'
-				rectwidth = 10
-				rectheight = 10
-			elif value == 'food':
-				value = 'barrier'
-			print(value)
-		if event.key == pygame.K_o:
-			rectwidth = rectwidth + rectheight
-			rectheight = rectwidth - rectheight
-			rectwidth = rectwidth - rectheight
-		if event.key == pygame.K_f:
-			print("Saving..")
-			csvfile = open(levelname + ".csv","w", newline='')
-			file_writer = csv.writer(csvfile)
-			for tup in xy:
-				file_writer.writerow(tup)
-			csvfile.close()
-			print("Saved!")
-			exit = True
+			if event.key == pygame.K_p:
+				if arrowIncrement == 2:
+					arrowIncrement = 0
+				else:
+					arrowIncrement += 1
+				while x%arrowIncrements[arrowIncrement] != 0:
+					x += 1
+
+				while y%arrowIncrements[arrowIncrement] != 0:
+					y += 1
+
+			if event.key == pygame.K_LEFT:
+				x += -arrowIncrements[arrowIncrement]
+				#print(str(x)+", "+str(y))
+			elif event.key == pygame.K_RIGHT:
+				x += arrowIncrements[arrowIncrement]
+				#print(str(x)+", "+str(y))
+			elif event.key == pygame.K_UP:
+				y += -arrowIncrements[arrowIncrement]
+				#print(str(x)+", "+str(y))
+			elif event.key == pygame.K_DOWN:
+				y += arrowIncrements[arrowIncrement]
+			if event.key == pygame.K_c:
+				if (value,x,y,rectwidth,rectheight) not in xy:
+					xy.append((value,x,y,rectwidth,rectheight))
+					print("Block saved to list")
+			if event.key == pygame.K_r:
+				while not accept:
+					try:
+						print("Resize block: ")
+						rectwidth = int(input("Enter rect width: "))
+						rectheight = int(input("Enter rect height: "))
+						accept = True
+					except:	
+						print("Invalid!")
+				accept = False
+			if event.key == pygame.K_z:
+				if len(xy) > 1:
+					print(str(xy.pop(-1))+" was removed from list")
+			if event.key == pygame.K_g:
+				if value == 'barrier':
+					value = 'food'
+					rectwidth = 10
+					rectheight = 10
+				elif value == 'food':
+					value = 'powerup'
+					rectwidth = 30
+					rectheight = 30
+				elif value == 'powerup':
+					value = barrier
+				print(value)
+			if event.key == pygame.K_o:
+				rectwidth = rectwidth + rectheight
+				rectheight = rectwidth - rectheight
+				rectwidth = rectwidth - rectheight
+			if event.key == pygame.K_f:
+				print("Saving..")
+				csvfile = open(levelname + ".csv","w", newline='')
+				file_writer = csv.writer(csvfile)
+				for tup in xy:
+					file_writer.writerow(tup)
+				csvfile.close()
+				print("Saved!")
+				exit = True
 	#if event.type == pygame.KEYUP:
 
 
 	screen.fill(black)		
 	for coord in xy:
-		pygame.draw.rect(screen,blue,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+		if coord[0] == "barrier":
+			pygame.draw.rect(screen,blue,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+		elif coord[0] == "food" or coord[0] == "powerup":
+			pygame.draw.rect(screen,white,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
 	if value == 'barrier':
 		pygame.draw.rect(screen,blue,[x,y,rectwidth,rectheight])
 	elif value == 'food':
+		pygame.draw.rect(screen,white,[x,y,rectwidth,rectheight])
+	elif value == 'powerup':
 		pygame.draw.rect(screen,white,[x,y,rectwidth,rectheight])
 	pygame.draw.rect(screen,yellow,[width/2,height/2,20,20])
 	pygame.display.update()
--- Pacman Pygame/pacman.py
+++ Pacman Pygame/pacman.py
@@ -3,13 +3,14 @@ from pygame.locals import *
 import random
 import csv
 
-#pygame.init()
+pygame.init()
 
 width = 600 #This can change
 height = 600 #This can change
-playersize = 20 #This can change
+playersize = 20 
 ghostsize = 20
 fps = 15 #This can change
+powerupTime = 5
 clock = pygame.time.Clock()
 
 #Create display
@@ -36,11 +37,12 @@ pacmanRight = [pygame.image.load('pacmanSprites/pacmanRight.png'), pygame.image.
 pacmanUp = [pygame.image.load('pacmanSprites/pacmanUp.png'), pygame.image.load('pacmanSprites/pacmanUpD.png')]
 pacmanDown = [pygame.image.load('pacmanSprites/pacmanDown.png'), pygame.image.load('pacmanSprites/pacmanDownD.png')]
 
-pinkGhost = [pygame.image.load('pacmanSprites/pinkLeft.png'),pygame.image.load('pacmanSprites/pinkRight.png'),pygame.image.load('pacmanSprites/pinkUp.png'),pygame.image.load('pacmanSprites/pinkDown.png')] 
-orangeGhost = [pygame.image.load('pacmanSprites/orangeLeft.png'),pygame.image.load('pacmanSprites/orangeRight.png'),pygame.image.load('pacmanSprites/orangeUp.png'),pygame.image.load('pacmanSprites/orangeDown.png')] 
-redGhost = [pygame.image.load('pacmanSprites/redLeft.png'),pygame.image.load('pacmanSprites/redRight.png'),pygame.image.load('pacmanSprites/redUp.png'),pygame.image.load('pacmanSprites/redDown.png')] 
-blueGhost = [pygame.image.load('pacmanSprites/blueLeft.png'),pygame.image.load('pacmanSprites/blueRight.png'),pygame.image.load('pacmanSprites/blueUp.png'),pygame.image.load('pacmanSprites/blueDown.png')] 
+pinkGhost = [pygame.image.load('pacmanSprites/pinkLeft.png'),pygame.image.load('pacmanSprites/pinkRight.png'),pygame.image.load('pacmanSprites/pinkUp.png'),pygame.image.load('pacmanSprites/pinkDown.png'),pygame.image.load('pacmanSprites/vulnerable.png')] 
+orangeGhost = [pygame.image.load('pacmanSprites/orangeLeft.png'),pygame.image.load('pacmanSprites/orangeRight.png'),pygame.image.load('pacmanSprites/orangeUp.png'),pygame.image.load('pacmanSprites/orangeDown.png'),pygame.image.load('pacmanSprites/vulnerable.png')] 
+redGhost = [pygame.image.load('pacmanSprites/redLeft.png'),pygame.image.load('pacmanSprites/redRight.png'),pygame.image.load('pacmanSprites/redUp.png'),pygame.image.load('pacmanSprites/redDown.png'),pygame.image.load('pacmanSprites/vulnerable.png')] 
+blueGhost = [pygame.image.load('pacmanSprites/blueLeft.png'),pygame.image.load('pacmanSprites/blueRight.png'),pygame.image.load('pacmanSprites/blueUp.png'),pygame.image.load('pacmanSprites/blueDown.png'),pygame.image.load('pacmanSprites/vulnerable.png')] 
 
+#status of player movement
 
 steps = 0
 left = False
@@ -56,45 +58,59 @@ pacY = height/2
 yChange = 0
 xChange = 0
 
+#starting coordinates of ghosts
+
 ghostCoords = {"blue":[width/2-50,height/2-50,blueGhost,0], "orange":[width/2-50,height/2-50,orangeGhost,0], "red":[width/2-50,height/2-50,redGhost,0], "pink":[width/2-50,height/2-50,pinkGhost,0] }
 
 score = 0
 
+#lists in order to build the level
 
 xy = []
 blocks = []
 barriers = []
 food = []
+powerups = []
 levelname = (input("Levelname (omit file extension):"))
 
-#if true then no entry
+#if true then cannot move in that direction 
 touching = False
-
+#if true then the ghosts are vulnerable 
+powerup = False
+powerupTimer = 0
 #if true then system exit
 gameExit = False
+
+#read level csv and add rect parameters to list
 
 csvfile =open(levelname +".csv",'r')
 obj=csv.reader(csvfile)
 for row in obj:
 	blocks.append((row))
 
-blocks[0][0] = pacX
-blocks[0][1] = pacY 
-
 for block in blocks:
 	if block[0] == "barrier":
 		barriers.append(block)
 	elif block[0] == "food":
 		food.append(block)
+	elif block[0] == "powerup":
+		powerups.append(block)
 print(barriers)
 print(food)
+print(powerups)
 
-def buildlevel(xy,screen,bcolor,fcolor):
-	global ghostCoords
+	#draw 
+
+	#iterate through the level csv and draw.rect() each of the pellet and barrier blocks
+def buildlevel(xy,screen,bcolor,fcolor,pcolor):
 	for coord in barriers:
 		pygame.draw.rect(screen,bcolor,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
 	for coord in food:
 		pygame.draw.rect(screen,fcolor,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+	for coord in powerups:
+		pygame.draw.rect(screen,pcolor,[float(coord[1]),float(coord[2]),float(coord[3]),float(coord[4])])
+
+	#one function to draw all of the sprites and buildlevel()
 
 def drawSprites():
 	DISPLAY.fill(grey)
@@ -106,9 +122,9 @@ def drawSprites():
 			steps = 0
 		DISPLAY.blit(pacmanLeft[steps],(pacX,pacY))
 	if right:
-		
 		if steps + 1 <= 1:
-			steps += 1
+			if not touching:
+				steps += 1
 		else:
 			steps = 0
 		DISPLAY.blit(pacmanRight[steps],(pacX,pacY))
@@ -125,6 +141,10 @@ def drawSprites():
 		else:
 			steps = 0
 		DISPLAY.blit(pacmanDown[steps],(pacX,pacY))
+
+
+
+	#draw and move ghosts around
 	
 	for ghost in ghostCoords:
 		ghostDirection = None
@@ -138,25 +158,31 @@ def drawSprites():
 		else:
 			for i in range(randGhostY):
 				ghostCoords[ghost][1] += (1*randGhostY)
-		
-		if randGhostX <= 0:
-			ghostDirection = 0
-		elif randGhostX >= 0:
-			ghostDirection = 1
-		elif randGhostY >= 0:
-			ghostDirection = 2
-		elif randGhostY <= 0:
-			ghostDirection = 3
+		if not powerup:
+			if randGhostX <= 0:
+				ghostDirection = 0
+			elif randGhostX >= 0:
+				ghostDirection = 1
+			elif randGhostY >= 0:
+				ghostDirection = 2
+			elif randGhostY <= 0:
+				ghostDirection = 3
+		else:
+			ghostDirection = 4
 
 
-		print(f"X of {ghost}:{ghostCoords[ghost][0]}")
-		print(f"Y of {ghost}:{ghostCoords[ghost][1]}")
+		#print(f"X of {ghost}:{ghostCoords[ghost][0]}")
+		#print(f"Y of {ghost}:{ghostCoords[ghost][1]}")
 
 		DISPLAY.blit(ghostCoords[ghost][2][ghostDirection], (ghostCoords[ghost][0], ghostCoords[ghost][1]))
 		
 	
-	buildlevel(xy,DISPLAY,darkBlue,white)
+	buildlevel(xy,DISPLAY,darkBlue,white,white)
 	pygame.display.update()
+
+def gameOver():
+	gameExit = True
+	sys.exit()
 	
 maxscore = len(food)
 
@@ -249,15 +275,41 @@ while gameExit == False:
 			score += 1 
 			food.remove(coord)
 			print(score)
+
+	for coord in powerups:
+		x = float(coord[1])
+		y = float(coord[2])
+		rectwidth = float(coord[3])
+		rectheight = float(coord[4])
+
+		#if the player touches a powerup, start a 5 second timer
+
+		if pacX+xChange > round(x-playersize) and pacX+xChange < round(x+rectwidth) and pacY+yChange > round(y-playersize) and pacY+yChange < round(y+rectheight):
+			powerup = True
+			powerupTimer = 0
+			powerups.remove(coord)
+
+	#if the timer time is up, turn off the powerup and reset the timer. Else, increa,pygame.image.load('pacmanSprites/vulnerable.png')se the timer by one.
+	if powerup == True and powerupTimer >= (fps*powerupTime):
+		powerup = False
+		powerupTimer = 0
+	elif powerup == True and powerupTimer < (fps*powerupTime):
+		powerupTimer += 1
+		print(powerupTimer//fps)
+
 	
-#is player touching a ghost?
+#is player touching a ghost? If so, if the powerup is active, reset the ghosts positions. If the powerup is inactive, end the game.
 	for ghost in ghostCoords:
 		x = ghostCoords[ghost][0]
 		y = ghostCoords[ghost][1]
 		rectwidth = ghostsize
 		rectheight = ghostsize
 		if pacX+xChange > round(x-playersize) and pacX+xChange < round(x+rectwidth) and pacY+yChange > round(y-playersize) and pacY+yChange < round(y+rectheight):
-			gameExit = True
+			if powerup == True:
+				ghostCoords[ghost][0] = width/2
+				ghostCoords[ghost][1] = height/2
+			else:
+				gameOver()
 		
 		
 	"""
